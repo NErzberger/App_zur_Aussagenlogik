5e4e59e5b765deaab75a321a14c1b38e269eaa9e
NErzberger <36168374+NErzberger@users.noreply.github.com>
2022-02-11 15:14:27 +0100

    Tastatur mit Funktionen belegt
    Alle Buttons der Tastatur haben in der MainActivity eine On-Click Implementierung bekommen.
    Es wurde über ein Modi Enum der Modus der App in der MainActivity eingestellt. Dieser wird über den Tab getriggert und über das TabSelected Event in der MainActivity implementiert.

--- app/src/main/java/com/dhbw/app_zur_aussagenlogik/MainActivity.java
+++ app/src/main/java/com/dhbw/app_zur_aussagenlogik/MainActivity.java
@@ -3,12 +3,267 @@ package com.dhbw.app_zur_aussagenlogik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout layout;
+
+    private TabItem item1;
+    private TabItem item2;
+    private TabItem item3;
+    private TabItem item4;
+    private TabItem item5;
+
+    private EditText inputText;
+
+    private Button buttonA;
+    private Button buttonB;
+    private Button buttonC;
+    private Button buttonD;
+    private Button buttonE;
+    private Button buttonNegation;
+    private Button buttonDelete;
+    private Button buttonCE;
+    private Button buttonAnd;
+    private Button buttonOr;
+    private Button buttonImplikation;
+    private Button buttonImplikationBeidseitig;
+    private Button buttonReturn;
+    private Button buttonKlammerAuf;
+    private Button buttonKlammerZu;
+
+    private Modi modus = Modi.DNF;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.tabLayout);
+        /*item1 = findViewById(R.id.tabItem1);
+        item2 = findViewById(R.id.tabItem2);
+        item3 = findViewById(R.id.tabItem3);
+        item4 = findViewById(R.id.tabItem4);
+        item5 = findViewById(R.id.tabItem5);*/
+
+        // Deklaration der Tastatur
+
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonD = findViewById(R.id.buttonD);
+        buttonE = findViewById(R.id.buttonE);
+        buttonNegation = findViewById(R.id.buttonNegaition);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonCE = findViewById(R.id.buttonCE);
+        buttonAnd = findViewById(R.id.buttonAnd);
+        buttonOr = findViewById(R.id.buttonOr);
+        buttonImplikation = findViewById(R.id.buttonImplikation);
+        buttonImplikationBeidseitig = findViewById(R.id.buttonImplikationBeidseitig);
+        buttonReturn = findViewById(R.id.buttonReturn);
+        buttonKlammerAuf = findViewById(R.id.buttonKlammerAuf);
+        buttonKlammerZu = findViewById(R.id.buttonKlammerZu);
+
+        inputText = findViewById(R.id.input);
+
+        // OnClickListener für das TabLayout
+
+        layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                String tabName = tab.getText().toString();
+                switch (tabName) {
+                    case "DNF":
+                        modus = Modi.DNF;
+                        break;
+                    case "KNF":
+                        modus = Modi.KNF;
+                        break;
+                    case "Resolution":
+                        modus = Modi.RESOLUTION;
+                        break;
+                    case "2 Formeln":
+                        modus = Modi.FORMELN;
+                        break;
+                    case "Tableaux":
+                        modus = Modi.TABLEAUX;
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+
+        });
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "a");
+            }
+        });
+
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "b");
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "c");
+            }
+        });
+
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "d");
+            }
+        });
+
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "e");
+            }
+        });
+
+        buttonNegation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "\u00AC");
+            }
+        });
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String formular = inputText.getText().toString();
+                inputText.setText(formular.substring(0, formular.length() - 1));
+            }
+        });
+
+        buttonCE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText("");
+            }
+        });
+
+        buttonAnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "\u2227");
+            }
+        });
+
+        buttonOr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "\u22C1");
+            }
+        });
+
+        buttonImplikation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "\u2192");
+            }
+        });
+
+        buttonImplikationBeidseitig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "\u2194");
+            }
+        });
+
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchParser();
+            }
+        });
+
+        buttonKlammerAuf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + "(");
+            }
+        });
+
+        buttonKlammerZu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(inputText.getText() + ")");
+            }
+        });
     }
+
+    private void launchParser(){
+        Parser parser = Parser.getInstance();
+        parser.setParserParameter(modus, inputText.getText().toString());
+    }
+
+/*
+
+Button reset = (Button) view.findViewById(R.id.reset);
+reset.setOnClickListener(this);
+
+@Override
+public void onClick(View v) {
+switch (v.getId()){
+
+        item1.setOnClickListener(view -> {
+            modus = Modi.DNF;
+            System.out.println("Ich setze den Modus DNF.");
+        });
+
+
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modus = Modi.KNF;
+                System.out.println("Ich setze den Modus KNF.");
+            }
+        });
+
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modus = Modi.RESOLUTION;
+                System.out.println("Ich setze den Modus Resolution.");
+            }
+        });
+
+        item4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modus = Modi.FORMELN;
+                System.out.println("Jetzt möchte ich zwei Formeln vergleichen.");
+            }
+        });
+
+        item5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modus = Modi.TABLEAUX;
+                System.out.println("TABLEAUX.");
+            }
+        });
+        */
+
+
 }
