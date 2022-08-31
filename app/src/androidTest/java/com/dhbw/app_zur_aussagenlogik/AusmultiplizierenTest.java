8d128e32dda8ce766549c5fb5a879615ace9dbaf
LauraM2 <60347067+LauraM2@users.noreply.github.com>
2022-08-31 17:46:07 +0200

    Tests angepasst
    Tests funktionieren noch jedoch noch nicht

--- app/src/androidTest/java/com/dhbw/app_zur_aussagenlogik/AusmultiplizierenTest.java
+++ app/src/androidTest/java/com/dhbw/app_zur_aussagenlogik/AusmultiplizierenTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertArrayEquals;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.dhbw.app_zur_aussagenlogik.core.Ausmultiplizieren;
+import com.dhbw.app_zur_aussagenlogik.core.Formel;
 import com.dhbw.app_zur_aussagenlogik.core.Parser;
 
 import org.junit.Test;
@@ -12,67 +14,57 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class AusmultiplizierenTest {
-
-/*
-// klappt nicht
     @Test
     public void schonDNF() {
         Parser p = new Parser();
-        char[] formel = {'(', 'a', '*', 'b', ')', '+', '(', 'a', '*', 'c', ')'};
-        char[] expectedFormel = {'(', 'a', '*', 'b', ')', '+', '(', 'a', '*', 'c', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel);
-        assertArrayEquals(expectedFormel, ausmultipliziert);
-
+        Formel formel = new Formel("(a*b)+(a*c)");
+        Formel expectedFormel = new Formel("(a*b)+(a*c)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
     }
 
     @Test
     public void schonDNF2() {
         Parser p = new Parser();
-        char[] formel = {'a', '+', '(', 'b', '*', 'c', ')'};
-        char[] expectedFormel = {'a', '+', '(', 'b', '*', 'c', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel);
-        assertArrayEquals(expectedFormel, ausmultipliziert);
-
+        Formel formel = new Formel("a+(b*c)");
+        Formel expectedFormel = new Formel("a+(b*c)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
     }
-
 
     @Test
     public void einfacherTest(){
         Parser p = new Parser();
-        char[] formel = {'a','*', '(', 'b', '+', 'c', ')'};
-        char[] expectedFormel = {'(', 'a','*', 'b', ')', '+', '(', 'a', '*', 'c', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel);
-        assertArrayEquals(expectedFormel, ausmultipliziert);
+        Formel formel = new Formel("a*(b+c)");
+        Formel expectedFormel = new Formel("(a*b)+(a*c)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
     }
 
     @Test
     public void zweiKlammernTest(){
         Parser p = new Parser();
-        char[] formel = {'(', 'a', '+', 'b', ')', '*', '(', 'b', '+', 'c', ')'};
-        char[] expectedFormel = {'(', 'a','*', 'b', ')', '+', '(', 'a', '*', 'c', ')', '+', '(', 'b', '*', 'b', ')', '+', '(', 'b', '*', 'c', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel);
-        assertArrayEquals(expectedFormel, ausmultipliziert);
-        // b* b muss noch rausfallen
+        Formel formel = new Formel("(a+b)*(b+c)");
+        Formel expectedFormel = new Formel("(a*b)+(a*c)+(b*c)+(b*b)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
     }
-
 
     @Test
     public void zweiKlammern4VarTest() {
         Parser p = new Parser();
-        char[] formel = {'(', 'a', '+', 'b', ')', '*', '(', 'c', '+', 'd', ')'};
-        char[] expectedFormel = {'(', 'a', '*', 'c', ')', '+', '(', 'a', '*', 'd', ')', '+', '(', 'b', '*', 'c', ')', '+', '(', 'b', '*', 'd', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel);
-        assertArrayEquals(expectedFormel, ausmultipliziert);
-
+        Formel formel = new Formel("(a+b)*(c+d)");
+        Formel expectedFormel = new Formel("(a*c)+(a*d)+(b*c)+(b*d)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
     }
 
-    //funktioniert noch nicht
     @Test
     public void zweiEinfacheUnd() {
         Parser p = new Parser();
-        char[] formel = {'d', '*', 'a', '*', '(', 'b', '+', 'c', ')'};
-        char[] expectedFormel = {'(', 'd', '*', 'a', '*', 'b', ')', '+', '(','(', 'a', '*', 'c', '*', 'd', ')', ')'};
-        char[] ausmultipliziert = p.ausmultiplizieren(formel); //{'(', 'd', '*', 'a', ')', '+', '(', 'd', '*', 'b', ')', '+', '(','d',  '*', 'c', ')'};
-        assertArrayEquals(expectedFormel, ausmultipliziert);
-    }*/
+        Formel formel = new Formel("d*a*(b+c)");
+        Formel expectedFormel = new Formel("(d*a*b)+(a*c*d)");
+        char[] ausmultipliziert = Ausmultiplizieren.ausmultiplizieren(formel).getFormel();
+        assertArrayEquals(expectedFormel.getFormel(), ausmultipliziert);
+    }
 }
