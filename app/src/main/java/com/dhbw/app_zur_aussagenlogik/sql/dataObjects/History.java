a0cde2bffcc7433cea2c223fbb8f8cb16de58efb
Nico Erzberger <36168374+NErzberger@users.noreply.github.com>
2022-09-22 13:52:12 +0200

    Bearbeitung der Normalform und weitere Kommentare

--- app/src/main/java/com/dhbw/app_zur_aussagenlogik/sql/dataObjects/History.java
+++ app/src/main/java/com/dhbw/app_zur_aussagenlogik/sql/dataObjects/History.java
@@ -1,5 +1,39 @@
 package com.dhbw.app_zur_aussagenlogik.sql.dataObjects;
 
+/**
+ * Die Klasse <b>History</b> wird dazu verwendet, um Formeln und deren Berechnungen in einem Verlauf zu speichern.
+ * Die Klasse erfüllt hierfür zwei Funktionen:
+ * <ol>
+ *     <li>Sie dient als Entity für die Datenbank</li>
+ *     <li>Objekt in den Fragments zum Laden der Formeln und des zugehörigen Modus</li>
+ * </ol>
+ * <br>
+ * Es werden insgesamt 4 Felder verwendet:
+ * <table>
+ *     <tr>
+ *         <th>Datentyp</th>
+ *         <th>Bezeichnung</th>
+ *     </tr>
+ *     <tr>
+ *         <td>int</td>
+ *         <td>id</td>
+ *     </tr>
+ *     <tr>
+ *         <td>String</td>
+ *         <td>modi</td>
+ *     </tr>
+ *     <tr>
+ *         <td>String</td>
+ *         <td>formula</td>
+ *     </tr>
+ *     <tr>
+ *         <td>String</td>
+ *         <td>secondFormula</td>
+ *     </tr>
+ * </table>
+ * @author Nico Erzberger
+ * @version 1.0
+ */
 public class History {
 
     private int id;
